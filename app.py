e36a81e8bc33f4fdd2bbf7bc07d41fc71b1e97bc
111deepak111 <deepaksinghpanwar111@gmail.com>
2024-03-07 05:51:02 +0530

    hello

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ flowers=pd.DataFrame(['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips'])
 flowers.columns=['Label']
 st.table(flowers)
 
-sunflower_path = ["./d1.jpg","./d2.jpg","./d3.jpg","./d4.jpg","./t1.jpg","./t2.jpg","./t3.jpg","./s1.jpg"]
+# sunflower_path = ["./d1.jpg","./d2.jpg","./d3.jpg","./d4.jpg","./t1.jpg","./t2.jpg","./t3.jpg","./s1.jpg"]
 img_height = 180
 img_width = 180
-for i in sunflower_path:
-    img = tf.keras.utils.load_img(
-        i, target_size=(img_height, img_width)
-    )
+
+def pred(img):
 
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
     print(score)
+    print("\n")
     score=flowers['Label'][np.argmax(score)]
     print(score)
+    print("\n\n\n")
